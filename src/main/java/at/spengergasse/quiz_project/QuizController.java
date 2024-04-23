741f694481486d27e7c5139cb802db7996dec03a
Markus Haider <hai210437@spengergasse.at>
2024-04-23 17:13:41 +0200

    quiz_main and little QuizController

--- src/main/java/at/spengergasse/quiz_project/QuizController.java
+++ src/main/java/at/spengergasse/quiz_project/QuizController.java
@@ -6,27 +6,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/quiz")
 public class QuizController {
     private QuestionRepository repo;
+    private int id = 1;
+    private int score = 0;
 
     public QuizController(QuestionRepository repo) {
         this.repo = repo;
     }
 
-    @GetMapping("/test")
+
+    @GetMapping("/main")
     public String test(Model model) {
-        System.out.println("Test wurde aufgerufen");
-        Question q = repo.findById(1).get();
-        model.addAttribute("test_attribute", q);
-        return "quiz_test";
+        Question q = repo.findById(id).get();
+        model.addAttribute("question", q);
+        return "quiz_main";
     }
 
     @PostMapping("/save")
-    public String save(Question question) {
-        System.out.println("Text geändert auf: " + question.getText());
-        repo.save(question);
-        return "redirect:/quiz/test";
+    public String save(Question question, int answer) {
+        id++;
+        System.out.println(answer);
+        for (Answer a : question.getAnswers()) {
+            {
+
+            }
+
+        }
+        return "redirect:/quiz/main";
     }
 }
+
